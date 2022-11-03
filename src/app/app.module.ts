00b03f37b832f2b59b0af427bb630501465785d4
arunmg83 <arunmg83@gmail.com>
2022-11-03 23:30:47 +0530

    added the forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule,MatSliderModule} from '@angular/material'
+import { MatCardModule, MatSliderModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule } from '@angular/material'
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 
@@ -20,7 +21,12 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     MatSliderModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  projectList: any = [{ name: 'Project 134', id: Date.now(), image: '../../assets/Images/OIP.jfif' }, { name: 'Project 1', id: Date.now(), image: '../../assets/Images/OIP.jfif' }, { name: 'Project 9', id: Date.now(), image: '../../assets/Images/OIP.jfif' }];
+  isAddProject: boolean = false;
+  form: FormGroup
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      projectName: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit() {
+  }
+  saveDetails(form) {
+    this.projectList.push({ name: form.value.projectName, image: '../../assets/Images/OIP.jfif' })
+    // this.form.setValue({ projectName: '' });
+    this.form.reset()
+
   }
 
 }
